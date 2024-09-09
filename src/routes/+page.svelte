3bbd14b01cbfa3b4ab14464e2d0fbd5ca4dd46c2
ndu <chijindujulian@gmail.com>
2024-09-09 18:28:17 +0100

    chore(fix): hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,12 @@
 
 <section class="hero px-6 lg:px-32 pt-64  min-h-screen">
     <h1 class="text-white font-interBold text-8xl pb-12">We are the <br> 2077 Collective.</h1>
-    <p class="text-white font-interMedium text-xl text-gray-300">Nosotros somos el Colectivo 2077.</p>
-    <p class="text-white font-interMedium text-xl text-gray-400">Wir sind das 2077 Kollektiv.</p>
-    <p class="text-white font-interMedium text-xl text-gray-500">我们是 2077 集体。</p>
-    <p class="text-white font-interMedium text-xl text-gray-600">Мы Коллектив 2077.</p>
-    <p class="text-white font-interMedium text-xl text-gray-700">نحن مجموعة 2077.</p>
-    <div class="pt-36 text-white">
+    <p class="text-white font-interMedium text-xl text-slate-300">Nosotros somos el Colectivo 2077.</p>
+    <p class="text-white font-interMedium text-xl text-slate-400">Wir sind das 2077 Kollektiv.</p>
+    <p class="text-white font-interMedium text-xl text-slate-500">我们是 2077 集体。</p>
+    <p class="text-white font-interMedium text-xl text-slate-600">Мы Коллектив 2077.</p>
+    <p class="text-white font-interMedium text-xl text-slate-700">نحن مجموعة 2077.</p>
+    <div class="pt-36 text-white ">
         <p class="text-3xl font-interSemi max-w-[680px]">
             We are creatives, researchers, marketers, and developers all working to make <span class="inline-block"><Eth /></span> Ethereum <s>cool</s> again.
         </p>
