e4b950bcebf10e58002487f55441fde25eb7ad86
ndu <chijindujulian@gmail.com>
2024-09-09 20:04:25 +0100

    chore(update): add beliefs section

---
+++ src/lib/components/icons/Values.svelte
@@ -0,0 +1,15 @@
+<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <rect width="24" height="24" rx="12" fill="#0B0B0B" />
+    <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M5.59453 16.4539L7.571 18.3918L18.4056 7.54606L16.4286 5.60808L5.59453 16.4539Z"
+        fill="white"
+    />
+</svg>
