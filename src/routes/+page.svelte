f9e34f737e7e7f8342bde86e471591febd8d9c4e
ndu <chijindujulian@gmail.com>
2024-09-11 13:43:09 +0100

    chore(update): add hover effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -327,21 +327,21 @@
 
         <div class="flex flex-col sm:flex-row gap-x-6 gap-y-6">
             <a
-                class="max-w-40 border border-white rounded-full py-4 px-6 text-white flex gap-x-4"
+                class="max-w-40 border border-white rounded-full py-4 px-6 text-white hover:opacity-65 flex gap-x-4"
                 href="https://discord.gg/2077collective" target="_blank"
             >
                 <Discord />
                 Discord
             </a>
             <a
-                class="max-w-40 border border-white rounded-full py-4 px-6 text-white flex gap-x-4"
+                class="max-w-40 border border-white rounded-full py-4 px-6 text-white hover:opacity-65 flex gap-x-4"
                 href="https://x.com/2077Collective" target="_blank"
             >
                 <Twitter />
                 Follow us
             </a>
             <a
-                class="max-w-40 border border-white rounded-full py-4 px-6 text-white flex gap-x-4"
+                class="max-w-40 border border-white rounded-full py-4 px-6 text-white hover:opacity-65 flex gap-x-4"
                 href="https://www.farcaster.id/c/2077collective" target="_blank"
             >
                 <Farcaster />
