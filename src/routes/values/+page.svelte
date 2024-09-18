213b2ec35c051a06437d57febd14b0d78d32e315
ndu <chijindujulian@gmail.com>
2024-09-19 00:29:08 +0100

    chore(update): add values-page

---
+++ src/routes/values/+page.svelte
@@ -0,0 +1,92 @@
+<section class="px-6 lg:px-20 pt-12 pb-8">
+    <div class="container flex flex-col gap-y-16 pt-20 pb-32">
+        <h1 class="font-bold text-5xl lg:text-8xl">Our values</h1>
+        <div class="flex flex-col gap-y-4">
+            <h2 class="text-4xl font-bold tracking-tight">Flexibility</h2>
+            <p class="text-2xl max-w-screen-5xl">
+                We are not a traditional organization with bells and whistles.
+                Crypto makes it possible for Internet strangers to coordinate
+                with strangers on the Internet and pull off impressive feats. We
+                provide maximum flexibility for contributors: the only
+                requirement is to operate and deliver consistently.
+            </p>
+        </div>
+        <div class="flex flex-col gap-y-4">
+            <h2 class="text-4xl font-bold tracking-tight">
+                Open global participation
+            </h2>
+            <p class="text-2xl max-w-screen-5xl">
+                Anyone in the world should be able to participate—irrespective
+                of their background, affiliations, and philosophies—on maximally
+                equal footing. Like Ethereum itself, participation in the
+                Collective is permissionless and open to anyone with a desire to
+                contribute positively to the Ethereum ecosystem.
+            </p>
+        </div>
+        <div class="flex flex-col gap-y-4">
+            <h2 class="text-4xl font-bold tracking-tight">
+                Meritocracy > bureaucracy
+            </h2>
+            <p class="text-2xl max-w-screen-5xl">
+                We lean into anon culture and emphasize operating vs.
+                performing, positive-sum games vs. zero-sum games, legitimacy by
+                performance vs. legitimacy by continuity, vibrancy vs.
+                sterility, and flexible structures vs. middle management and
+                bureaucratic processes. The pie is big enough, and those who
+                grow the pie must naturally have a (larger) share in it.
+            </p>
+        </div>
+        <div class="flex flex-col gap-y-4">
+            <h2 class="text-4xl font-bold tracking-tight">Sovereignty</h2>
+            <p class="text-2xl max-w-screen-5xl">
+                Autonomy, self-leadership, and personal independence are
+                necessary for innovation and creativity to thrive. We give room
+                for the risk-takers, innovators, and operators to flourish and
+                do their best work without stifling their freedom or raising the
+                barrier to action.
+            </p>
+        </div>
+        <div class="flex flex-col gap-y-4">
+            <h2 class="text-4xl font-bold tracking-tight">
+                Creativity and authenticity
+            </h2>
+            <p class="text-2xl max-w-screen-5xl">
+                We're dreamers who believe in powerful ideas and their ability
+                to change the world. We experiment with radical approaches and
+                bet against the consensus—our efforts are geared towards playing
+                long-term games and maximizing ROI on high-conviction plays. We
+                allow for self-expression and believe work can and should be an
+                avenue for people to create beautiful things that are
+                indistinguishable from art.
+            </p>
+        </div>
+        <div class="flex flex-col gap-y-4">
+            <h2 class="text-4xl font-bold tracking-tight">Speed</h2>
+            <p class="text-2xl max-w-screen-5xl">
+                The speed limit doesn't exist, and the standard pace is for
+                chumps. At 2077 Collective, moving fast and making things—whilst
+                honoring our commitment to quality—is a credo that influences
+                our short-term and long-term actions. We're big believers in
+                maintaining a strong bias toward action and prioritizing (rapid)
+                experimentation and execution over (prolonged) analysis and
+                deliberation.
+            </p>
+        </div>
+        <div class="flex flex-col gap-y-4">
+            <h2 class="text-4xl font-bold tracking-tight">
+                Think and act like winners
+            </h2>
+            <p class="text-2xl max-w-screen-5xl">
+                We play for keepers and go all in. We don't play the odds or
+                second-guess our chances of success. If no path exists towards
+                achieving our most ambitious goals, we will make one—no obstacle
+                is too large, no setback too damaging, and nothing enough to
+                break our will or destroy our resolve to succeed in the market.
+                We're fearless, persistent, aggressive, relentless and
+                courageous; we're biased towards displaying and enacting radical
+                competence and winning based on merit and the quality of our
+                efforts.
+            </p>
+        </div>
+    </div>
+</section>
